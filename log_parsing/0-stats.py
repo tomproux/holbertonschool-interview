b3f21c38f39215c0ca1a5096e1c1a043d2d042bc
tomproux <tomproux53@gmail.com>
2026-06-05 12:12:53 +0200

    ok

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     status_codes = {}
     
     # Pattern to match log format: IP - [date] "METHOD path HTTP/VERSION" status_code size
-    pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "(\w+) (.*?) (HTTP/\d\.\d)" (\d+) (\d+)$'
+    pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "(\w+) (.*?) (HTTP/[\d\.]+)" (\d+) (\d+)$'
     
     try:
         for line in sys.stdin:
