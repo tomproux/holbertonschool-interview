b92cf371284232193efa5e4d3ef05f58ad5b743d
tomproux <tomproux53@gmail.com>
2026-07-03 11:49:57 +0200

    ok

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Rotate a square 2D matrix 90 degrees clockwise in-place."""
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotate an n x n 2D matrix 90 degrees clockwise in-place.
